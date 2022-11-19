f922e0fc60697611f00879c4eacd4a3620c8f788
Gawvena <118579304+Gawvena@users.noreply.github.com>
2022-11-18 19:26:53 -0600

    Lab 12

---
+++ Lab12/account.py
@@ -0,0 +1,25 @@
+class Account:
+
+    def __init__(self, name):
+        self._account_name = name
+        self._account_balance = 0
+
+    def deposit(self, amount):
+        if amount >= 0:
+            self._account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if amount >= 0:
+            self._account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self._account_balance
+
+    def get_name(self):
+        return self._account_name
