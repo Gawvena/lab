37f80570b5ed123a00d11c83646ba93c707f8739
Gawvena <118579304+Gawvena@users.noreply.github.com>
2022-11-20 21:54:28 -0600

    fixes

--- Lab12/account.py
+++ Lab12/account.py
@@ -1,25 +1,25 @@
 class Account:
 
     def __init__(self, name):
-        self._account_name = name
-        self._account_balance = 0
+        self.__account_name = name
+        self.__account_balance = 0
 
     def deposit(self, amount):
         if amount >= 0:
-            self._account_balance += amount
+            self.__account_balance += amount
             return True
         else:
             return False
 
     def withdraw(self, amount):
-        if amount >= 0:
-            self._account_balance -= amount
+        if amount >= 0 or amount > self.__account_balance:
+            self.__account_balance -= amount
             return True
         else:
             return False
 
     def get_balance(self):
-        return self._account_balance
+        return self.__account_balance
 
     def get_name(self):
-        return self._account_name
+        return self.__account_name
