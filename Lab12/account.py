186084217d68d843c4e52c0b1db0033f7462e843
Gawvena <118579304+Gawvena@users.noreply.github.com>
2022-11-20 21:58:16 -0600

    fix2

--- Lab12/account.py
+++ Lab12/account.py
@@ -12,7 +12,7 @@ class Account:
             return False
 
     def withdraw(self, amount):
-        if amount >= 0 or amount > self.__account_balance:
+        if amount >= 0 or amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         else:
